39477f4afb1e883e2260dacf0ca5c5b6c2cf59d7
nelson509 <35314794+nelson509@users.noreply.github.com>
2022-10-21 18:22:05 -0700

    first commit with folders

---
+++ index.js
@@ -0,0 +1,28 @@
+// example from an Employee function 
+class Employee {
+    
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+  
+    getName() {
+      return this.name;
+    }
+  
+    getId() {
+      return this.id;
+    }
+  
+    getEmail() {
+      return this.email;
+    }
+  
+    getRole() {
+      return "Employee";
+    }
+  
+  }
+  
+  module.exports = Employee;
---
+++ src/page-template.js
@@ -0,0 +1,17 @@
+// set up variable to generate team 
+const buildTeam = team => {
+    const buildManager = (manager) => {
+        return ``
+        // build cards in HTML
+    }
+    const buildEngineer= (engineer) => {
+        return ``
+        // build cards in HTML
+    }
+    const buildIntern = (intern) => {
+        return ``
+        // build cards in HTML
+    }
+    // and so on for reach team members
+
+}
