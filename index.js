f42a4088d08842afd7d5fc6e49ae2b4d445b2105
nelson509 <35314794+nelson509@users.noreply.github.com>
2022-10-24 12:09:37 -0700

    added info to index.js

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-// example from an Employee function 
-class Employee {
+const inquirer = require('inquirer');
+const fs = require('fs');
+const pageTemplate = require('./src/page-template.js');
+
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        message: 'Please enter the name of the team manager',
+        name: 'manager'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the ID of the manager',
+        name: 'ID'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the email of the team manager',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the office number of the manager',
+        name: 'office'
+    },
+    {
+        type: 'list',
+        message: 'Choose a member for the team',
+        choices: ['Engineer', 'Intern'],
+        name: 'employee'
+    },
     
-    constructor(name, id, email) {
-      this.name = name;
-      this.id = id;
-      this.email = email;
-    }
-  
-    getName() {
-      return this.name;
-    }
-  
-    getId() {
-      return this.id;
-    }
-  
-    getEmail() {
-      return this.email;
-    }
-  
-    getRole() {
-      return "Employee";
-    }
-  
-  }
-  
-  module.exports = Employee;
+])
+.then((data) => {
+    const contentHTML = pageTemplate(data);
+
+    fs.writeFile('myTeam.html', contentHTML,
+    (err) => err ? console.log(err) : console.log("myTeam.html file successfully created")
+    );
+});
